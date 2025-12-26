a3ad807555cfe6ac51be1666d3fb72de741105e0
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2025-12-26 18:48:41 -0300

    feat: nova  função:  contador

--- ex001/script.js
+++ ex001/script.js
@@ -3,3 +3,19 @@
 const botao = document.getElementById('botaoContador')
 
 console.log(botao); // Vamos testar se pegamos o  botão
+
+// 2. CRIAR uma variável para guardar o número de cliques
+let contagem = 0;
+
+
+botao.addEventListener('click', function() {
+    console.log('Clicou no botão!')
+    // Aumenta o contador em 1
+    contagem ++
+    
+    // Atualiza o texto do botão
+
+    botao.textContent = 'Cliques:' + contagem
+
+    console.log('Contagem atual:', contagem)
+})
